9772fdbc6150329d6ab0ccac6407053e29294beb
suaoc <saleem.ullah.88@gmail.com>
2016-03-14 23:35:48 +0500

    bootstrap buttons for reload and clear
    bootstrap and jquery via npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,12 +11,24 @@ module.exports = function(grunt) {
         src: 'public/js/main.js',
         dest: 'site/dist/m.js'
       }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules/bootstrap/dist/css/',
+          src: ['bootstrap.css'],
+          dest: 'site/dist/css',
+          ext: '.min.css'
+        }]
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'cssmin']);
 };
